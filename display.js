cfe286f02c102924a8b57929616d6fe506609275
Alhadis <gardnerjohng@gmail.com>
2019-01-10 15:08:35 +1100

    Port "correct" glyph-rendering from illumos source
    Source: usr/src/common/font/font.c (illumos/illumos-gate@9890ff8)

--- display.js
+++ display.js
@@ -2,26 +2,67 @@
 "use strict";
 
 const Gallant_12x22 = require("./gallant12x22.js");
+const encoding      = require("./encoding.js");
 
-function parseFont(data, on = "█", off = " "){
-	const chars = [];
-	for(let i = 0; i < data.length; i += 44){
-		const char = [];
-		const c = data.slice(i, i + 44);
-		for(let j = 0; j < c.length; j += 2){
-			let l = c[j].toString(2);
-			let r = c[j+1].toString(2);
-			l = "0".repeat(8 - l.length) + l;
-			r = "0".repeat(8 - r.length) + r;
-			char.push((l + r).split("").map(c => +c ? on : off));
-		}
-		chars.push(char);
+function buildFontMap(data, encoding){
+	const font = {};
+	for(let i = 0; i < encoding.length; ++i){
+		const char = encoding[i];
+		font[char] = [];
+		for(let b = 0; b < 44; ++b)
+			font[char].push(data[(44 * i) + b]);
 	}
-	return chars;
+	const lowLine = String.fromCharCode(0x0332);
+	font[lowLine] = font[lowLine] || font["⎽"];
+	return font;
 }
 
-let data = parseFont(Gallant_12x22);
-data = data.map(char => {
-	return char.map(row => row.join("")).join("\n");
-}).join("\n\n");
-console.log(data);
+function render(char, font, options = {}){
+	const {
+		fgChar    = "█",
+		bgChar    = " ",
+		eolChar   = "\n",
+		underline = false,
+		
+		// These shouldn't be changed for this font
+		bytesWide   = 2,   //(f->vf_width + 7) / 8;
+		glyphWidth  = 12,  // f->vf_width
+		glyphHeight = 22,  // f->vf_height
+	} = options;
+	
+	let output  = "";
+	const glyph = font[char];
+	const ul = font[String.fromCharCode(0x0332)];
+	
+	let pixel = 0;
+	for(let row = 0; row < glyphHeight; ++row){
+		let bitsLeft = glyphWidth;
+		
+		for(let byte = 0; byte < bytesWide; ++byte){
+			let data = glyph[pixel];
+			if(underline)
+				data |= ul[pixel];
+			++pixel;
+			
+			let mask    = 0x80;
+			const nbits = Math.min(8, bitsLeft);
+			bitsLeft   -= nbits;
+			
+			for(let i = 0; i < nbits; ++i){
+				output += data & mask ? fgChar : bgChar;
+				mask >>= 1;
+			}
+		}
+		output += eolChar;
+	}
+	return output;
+}
+
+const options = {
+	bgChar: process.stdout.isTTY ? "\x1B[44m \x1B[0m" : " ",
+	underline: false,
+};
+
+const font = buildFontMap(Gallant_12x22, encoding);
+let a = encoding.map(c => render(c, font, options)).join("\n");
+console.log(a);
